614d4c4bda61c04b26a482c298c20bdc11f3d016
ledox <101424523+ledoxmedox@users.noreply.github.com>
2023-06-11 01:09:31 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -11,3 +11,19 @@ git clone https://github.com/ledoxmedox/pink
 spicetify config current_theme pink
 
 spicetify apply
+
+$destinationFolder = "$env:APPDATA\spicetify\themes\pink"
+
+$filesToDelete = @(
+    "install.ps1",
+    "README.md"
+)
+
+foreach ($file in $filesToDelete) {
+    $filePath = Join-Path -Path $destinationFolder -ChildPath $file
+
+    if (Test-Path $filePath -PathType Leaf) {
+        Remove-Item $filePath -Force
+        Write-Host "Deleted file: $file"
+    }
+}
