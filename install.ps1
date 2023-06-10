42a9f735d56cb398dc28f790165e8dc7d2c58494
ledox <101424523+ledoxmedox@users.noreply.github.com>
2023-06-11 00:42:45 +0700

    Add files via upload

--- install.ps1
+++ install.ps1
@@ -1,3 +1,9 @@
+$destinationFolder = "$env:APPDATA\spicetify\themes\pink"
+
+if (Test-Path $destinationFolder -PathType Container) {
+    Remove-Item $destinationFolder -Recurse -Force
+}
+
 Set-Location "$env:APPDATA\spicetify\themes"
 
 git clone https://github.com/ledoxmedox/pink
